0598dbae7cd81926f0d3b58b40fd4d830c97dd28
Noah Reeder <noahreederatc@gmail.com>
2026-07-02 00:23:06 -0500

    Fixed bug in mireo can utils with fixed width types
    Compiled fine on amd64, but failed on arm64. Switched to using proper header instead of hardcoded typedefs

--- ros2_j1939_babbler/third-party/mireo_can_utils/can/can_kernel.h
+++ ros2_j1939_babbler/third-party/mireo_can_utils/can/can_kernel.h
@@ -46,12 +46,7 @@
 #define CAN_KERNEL_H
 
 #include <stdint.h>
-
-typedef int32_t __s32;
-typedef uint32_t __u32;
-typedef uint8_t __u8;
-typedef uint16_t __u16;
-typedef uint64_t __u64;
+#include <linux/types.h>
 
 /* controller area network (CAN) kernel definitions */
 
